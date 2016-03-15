7e5048c6873fba6e3e9d2a509c7c9084d419c7d3
meeplings <asamoahj16@byramhills.net>
2016-03-15 08:58:20 -0400

    Update ThemeHandler.java

--- app/src/main/java/com/example/asamoahfamily/alzheimers/ThemeHandler.java
+++ app/src/main/java/com/example/asamoahfamily/alzheimers/ThemeHandler.java
@@ -5,14 +5,28 @@ import android.content.res.TypedArray;
 import android.util.TypedValue;
 
 public class ThemeHandler {
+    
+    private static final int[] YELLOW_THEME = {R.color.yPrime,  R.color.yLight, R.color.yDark,  R.color.yAccent,
+    R.color.divideDark, R.color.darkPrimeT};
+    
+    private static final int[] DARK_BLUE_THEME = {R.color.dBPrime,  R.color.dBLight, R.color.dBDark,  R.color.dBAccent,
+    R.color.divideLight, R.color.lightPrimeT};
+    
+    private static final int[] RED_THEME = {R.color.rPrime,  R.color.rLight, R.color.rDark,  R.color.rAccent,
+    R.color.divideDark, R.color.darkPrimeT};
+    
+    private static final int[] BROWN_THEME = {R.color.brPrime,  R.color.brLight, R.color.brDark,  R.color.brAccent,
+    R.color.divideLight, R.color.lightPrimeT};
+    
+    private static final int[] ORANGE_THEME = {R.color.oPrime,  R.color.oLight, R.color.oDark,  R.color.oAccent,
+    R.color.divideDark, R.color.darkPrimeT};
 
 
     private int mPrime,mDark,mLight,mAcc,mTextPrime,mTextSecond,mDiv;
-    private TypedValue readAttrs;
-
+    private int[] mCols;
     public ThemeHandler(){
-        readAttrs = new TypedValue();
-
+        mCols = new int[6];
+        
         mPrime = 0;
         mDark = 0;
         mLight = 0;
@@ -22,18 +36,35 @@ public class ThemeHandler {
         mDiv = 0;
     }
 
-    public void setThemeCols(Context c) {
-        c.getTheme().resolveAttribute(R.attr.colorPrime, readAttrs, true);
-        int[] themeCols = {R.attr.colorPrime,R.attr.colorDark,R.attr.colorLight,R.attr.colorAccent, R.attr.textColorPrimary, R.attr.textColorSecondary,R.attr.divide};
-        TypedArray colArray = c.obtainStyledAttributes(readAttrs.data, themeCols);
-        mPrime = colArray.getColor(0, 0);
-        mDark = colArray.getColor(1, 0);
-        mLight = colArray.getColor(2,0);
-        mAcc = colArray.getColor(3,0);
-        mTextPrime = colArray.getColor(4,0);
-        mTextSecond = colArray.getColor(5,0);
-        mDiv = colArray.getColor(6,0);
-        colArray.recycle();
+    public void setThemeCols(String t) {
+        
+        switch(t){
+            case "RED":
+                mCols = RED_THEME;
+                break;
+            case "DARK_BLUE":
+                mCols = DARK_BLUE_THEME;
+                break;
+            case "BROWN":
+                mCols = BROWN_THEME;
+                break;
+            case "YELLOW":
+                mCols = YELLOW_THEME;
+                break;
+            case "ORANGE":
+                mCols = "ORANGE_THEME";
+                break;
+            default:
+                //TODO: add error to stop thing
+        }
+        
+        mPrime = mCols[0];
+        mLight = mCols[1];
+        mDark = mCols[2];
+        mAcc = mCols[3];
+        mDiv = mCols[4];
+        mTextPrime = mCols[5];
+        mTextSecond = R.color.secondT;
     }
 
     public int getmPrime() {
